793de7a847ac4409a0b148ddc4bc9dfa214c8b6e
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2024-07-30 22:39:16 +0700

    feat: 6 digit token

--- src/helpers/randomTokenGenerate.ts
+++ src/helpers/randomTokenGenerate.ts
@@ -1,10 +1,10 @@
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const characters = '0123456789';
 
 const randomTokenGenerate = () => {
   let result = '';
   const charactersLength = characters.length;
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
 
