f10a17964a0aa1ab9440cdfaa12418384226a4fd
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2024-08-11 18:18:23 +0700

    fix: Add user object to validation
    so we can get the useer id in the service

--- src/validators/cash-transaction-validation.ts
+++ src/validators/cash-transaction-validation.ts
@@ -2,6 +2,15 @@ import { z, ZodType } from 'zod';
 
 export class CashTransactionValidation {
   static CREATE: ZodType = z.object({
+    user: z
+      .object({
+        id: z.string().uuid(),
+        full_name: z.string(),
+        email: z.string().email(),
+        account_number: z.string(),
+        balance: z.number()
+      })
+      .required(),
     amount: z.number().positive().int(),
     type: z.enum(['TOPUP', 'WITHDRAW'])
   });
