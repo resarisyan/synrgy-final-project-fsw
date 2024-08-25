a7f8ab4ac27f3e1f27d597aa8945feadd499d5ad
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2024-08-26 04:32:42 +0700

    fix: amount validation logic depends on trx type
    - when generate, "amount" is required for WITHDRAW transactions and not required for TOPUP
    - when demo (wd & tp), "amount" required for TOPUP transactions and not required for WITHDRAW

--- src/validators/cash-transaction-validation.ts
+++ src/validators/cash-transaction-validation.ts
@@ -1,13 +1,35 @@
 import { z, ZodType } from 'zod';
+import { ResponseError } from '../handlers/response-error';
 
 export class CashTransactionValidation {
-  static CREATE: ZodType = z.object({
-    amount: z.number().positive().int(),
-    type: z.enum(['TOPUP', 'WITHDRAW'])
-  });
+  static CREATE: ZodType = z
+    .object({
+      amount: z.number().positive().int().optional(),
+      type: z.enum(['TOPUP', 'WITHDRAW'])
+    })
+    .refine((data) => {
+      if (data.type === 'WITHDRAW' && data.amount === undefined) {
+        throw new ResponseError(
+          400,
+          'Amount is required for Withdraw transaction'
+        );
+      }
+      return true;
+    });
 
-  static STORE: ZodType = z.object({
-    token: z.string(),
-    type: z.enum(['TOPUP', 'WITHDRAW'])
-  });
+  static STORE: ZodType = z
+    .object({
+      amount: z.number().positive().int().optional(),
+      token: z.string(),
+      type: z.enum(['TOPUP', 'WITHDRAW'])
+    })
+    .refine((data) => {
+      if (data.type === 'TOPUP' && data.amount === undefined) {
+        throw new ResponseError(
+          400,
+          'Amount is required for Top Up transaction'
+        );
+      }
+      return true;
+    });
 }
