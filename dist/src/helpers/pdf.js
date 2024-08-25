d047961e103960778418105132aa3e7f3d5a157b
resarisyan <resarisyan77@gmail.com>
2024-08-25 12:08:17 +0700

    fix: pdf

--- dist/src/helpers/pdf.js
+++ dist/src/helpers/pdf.js
@@ -5,8 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.convertHTMLToPDF = convertHTMLToPDF;
 const puppeteer_1 = __importDefault(require("puppeteer"));
+const chromium_1 = __importDefault(require("@sparticuz/chromium"));
+const fs_1 = __importDefault(require("fs"));
 async function convertHTMLToPDF(htmlContent, pdfFilePath, margins = { top: '10mm', right: '10mm', bottom: '10mm', left: '10mm' }) {
-    const browser = await puppeteer_1.default.launch({});
+    const executablePath = await chromium_1.default.executablePath();
+    if (!fs_1.default.existsSync(executablePath)) {
+        throw new Error(`Chromium executable not found at path: ${executablePath}`);
+    }
+    const browser = await puppeteer_1.default.launch({
+        defaultViewport: chromium_1.default.defaultViewport,
+        executablePath: executablePath,
+        headless: chromium_1.default.headless
+    });
     const page = await browser.newPage();
     await page.setContent(htmlContent);
     await page.pdf({ path: pdfFilePath, format: 'A4', margin: margins });
--- src/helpers/pdf.ts
+++ src/helpers/pdf.ts
@@ -1,11 +1,20 @@
 import puppeteer from 'puppeteer';
-
+import chromium from '@sparticuz/chromium';
+import fs from 'fs';
 export async function convertHTMLToPDF(
   htmlContent: string,
   pdfFilePath: string,
   margins = { top: '10mm', right: '10mm', bottom: '10mm', left: '10mm' }
 ) {
-  const browser = await puppeteer.launch({});
+  const executablePath = await chromium.executablePath();
+  if (!fs.existsSync(executablePath)) {
+    throw new Error(`Chromium executable not found at path: ${executablePath}`);
+  }
+  const browser = await puppeteer.launch({
+    defaultViewport: chromium.defaultViewport,
+    executablePath: executablePath,
+    headless: chromium.headless
+  });
 
   const page = await browser.newPage();
   await page.setContent(htmlContent);
