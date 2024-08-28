e80a82e588615ef4947a04f084ed519d4224175c
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2024-08-28 19:55:49 +0700

    (fix) check phone middleware

--- src/middlewares/check-phone-middleware.ts
+++ src/middlewares/check-phone-middleware.ts
@@ -1,6 +1,7 @@
 import { Response, NextFunction } from 'express';
 import { UserModel } from '../models/UserModel';
 import { UserRequest } from '../dtos/request/user-request';
+import { CashTransactionModel } from '../models/CashTransactionModel';
 import { ResponseError } from '../handlers/response-error';
 import { errorResponse } from '../dtos/response/error-response';
 import { Validation } from '../validators';
@@ -12,13 +13,22 @@ export const checkPhoneMiddleware = async (
   next: NextFunction
 ) => {
   try {
+    const reqToken = req.body || {};
+    const token = reqToken.token;
+    const userId = await CashTransactionModel.query()
+      .findOne({ code: token })
+      .select('user_id');
+
+    const userID = JSON.parse(JSON.stringify(userId)).user_id;
     const request = Validation.validate(GeneralValidation.PHONE, req.body);
     const phone = request.phone;
 
-    const user = await UserModel.query().where('phone', phone).first();
-    req.user = user;
+    const user = await UserModel.query().findById(userID);
+    if (!user) {
+      throw new ResponseError(401, 'User not found');
+    }
 
-    const userPhone = user!.phone;
+    const userPhone = user.phone;
 
     const phoneValidation = phone === userPhone;
     if (!phoneValidation) {
